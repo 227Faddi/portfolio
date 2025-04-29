240dacf37f6111d844ccd5bb129176ce0defcd9c
227Faddi <falukhouma27@gmail.com>
2025-04-29 00:01:47 -0400

    chore: more about section on top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,6 +38,16 @@ export default function Page() {
           </div>
         </div>
       </section>
+      <section id="about">
+        <BlurFade delay={BLUR_FADE_DELAY * 3}>
+          <h2 className="text-xl font-bold">About Me</h2>
+        </BlurFade>
+        <BlurFade delay={BLUR_FADE_DELAY * 4}>
+          <Markdown className="prose max-w-full text-pretty font-sans text-sm text-muted-foreground dark:prose-invert">
+            {DATA.summary}
+          </Markdown>
+        </BlurFade>
+      </section>
       <section id="projects">
         <div className="space-y-12 w-full py-12">
           <BlurFade delay={BLUR_FADE_DELAY * 11}>
@@ -78,16 +88,6 @@ export default function Page() {
             ))}
           </div>
         </div>
-      </section>
-      <section id="about">
-        <BlurFade delay={BLUR_FADE_DELAY * 3}>
-          <h2 className="text-xl font-bold">About</h2>
-        </BlurFade>
-        <BlurFade delay={BLUR_FADE_DELAY * 4}>
-          <Markdown className="prose max-w-full text-pretty font-sans text-sm text-muted-foreground dark:prose-invert">
-            {DATA.summary}
-          </Markdown>
-        </BlurFade>
       </section>
       <section id="skills">
         <div className="flex min-h-0 flex-col gap-y-3">
